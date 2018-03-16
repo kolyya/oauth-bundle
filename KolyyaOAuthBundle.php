a21c3f0c80b1ef93038891ab9efe01170aeffa92
qwe <qwe@ubuntu>
2018-03-16 10:45:50 +0300

    bundles

--- KolyyaOAuthBundle.php
+++ KolyyaOAuthBundle.php
@@ -12,4 +12,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KolyyaOAuthBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = $this->createContainerExtension();
+        }
+
+        return $this->extension;
+    }
 }
